3745a562b73319f03ce8d635f883b8bead40402f
Jeganathan <jkarthike@gmail.com>
2021-04-25 11:52:32 +0200

    Code formatted

--- components/input/comments.js
+++ components/input/comments.js
@@ -5,27 +5,27 @@ import NewComment from './new-comment';
 import classes from './comments.module.css';
 
 function Comments(props) {
-  const { eventId } = props;
+    const { eventId } = props;
 
-  const [showComments, setShowComments] = useState(false);
+    const [showComments, setShowComments] = useState(false);
 
-  function toggleCommentsHandler() {
-    setShowComments((prevStatus) => !prevStatus);
-  }
+    function toggleCommentsHandler() {
+        setShowComments((prevStatus) => !prevStatus);
+    }
 
-  function addCommentHandler(commentData) {
-    // send data to API
-  }
+    function addCommentHandler(commentData) {
+        // send data to API
+    }
 
-  return (
-    <section className={classes.comments}>
-      <button onClick={toggleCommentsHandler}>
-        {showComments ? 'Hide' : 'Show'} Comments
-      </button>
-      {showComments && <NewComment onAddComment={addCommentHandler} />}
-      {showComments && <CommentList />}
-    </section>
-  );
+    return (
+        <section className={classes.comments}>
+            <button onClick={toggleCommentsHandler}>
+                {showComments ? 'Hide' : 'Show'} Comments
+            </button>
+            {showComments && <NewComment onAddComment={addCommentHandler} />}
+            {showComments && <CommentList />}
+        </section>
+    );
 }
 
 export default Comments;
